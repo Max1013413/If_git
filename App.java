52da91c51c85852add4c4cb3c63a930378a468d4
Max1013413 <amk1013413@student.hamk.fi>
2025-10-21 21:48:33 +0300

    Update App.java

--- App.java
+++ App.java
@@ -4,7 +4,7 @@ public class App {
     
 
         int luku1 = 33;
-        int luku2 = 22;
+        int luku2 = 33;
 
         if (luku1 == luku2) {
             System.out.println("luku1 ja luku2 yhtäsuuret");
