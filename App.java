3ea202dbf0295f66388067d61cc595a9d2a0a382
Max1013413 <amk1013413@student.hamk.fi>
2025-10-21 21:46:36 +0300

    Create App.java

---
+++ App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    
+
+        int luku1 = 9;
+        int luku2 = 2;
+
+        if (luku1 == luku2) {
+            System.out.println("luku1 ja luku2 yhtäsuuret");
+        }
+    
+        if (luku1 != luku2) {
+            System.out.println("luku1 ja luku2 ei yhtäsuuret");
+        }
+    }
+}
